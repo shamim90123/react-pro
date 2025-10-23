9947cd33271bab9b5bb64dff69e479a3a2ffd1e0
shamim reza <shamimreza@revinr.com>
2025-10-23 07:13:00 +0600

    style to assets

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
 import App from "./App.jsx";
-import "./styles/globals.css"; // ✅ Main CSS entry
+import "@/assets/styles/globals.css"; // ✅ Main CSS entry
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
